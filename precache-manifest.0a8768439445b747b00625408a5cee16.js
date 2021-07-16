9ebf81821e1744c07bda24224c74b3d6940de8ef
Ying <cy240705@gmail.com>
2021-07-15 21:46:04 -0700

    Updates

---
+++ precache-manifest.0a8768439445b747b00625408a5cee16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27da642c15f458e7669a80b83d2c2376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00bce34f21bba50db920",
+    "url": "/static/css/2.a2524b97.chunk.css"
+  },
+  {
+    "revision": "f57193e12c77ba334a22",
+    "url": "/static/css/main.9bcb1388.chunk.css"
+  },
+  {
+    "revision": "00bce34f21bba50db920",
+    "url": "/static/js/2.8b92cbee.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.8b92cbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57193e12c77ba334a22",
+    "url": "/static/js/main.67bf56b6.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
